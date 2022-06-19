4d657580a3d856bb9006427ed84327b1f811a231
6iw <74735976+amadich@users.noreply.github.com>
2022-06-19 19:36:35 +0100

    Update tv.js

--- public/lib/tv.js
+++ public/lib/tv.js
@@ -18,7 +18,7 @@ const config = {
         }
     },
     backgroundColor: "#75b298",
-    scene: [loading,menu_main,menu_choix]
+    scene: [{preload: preload,create:create},loading,menu_main,menu_choix]
 }
 let game = new Phaser.Game(config);
 let logo_menu;
@@ -27,3 +27,12 @@ let po;
 let particles_rain;
 let LoginInput_User;
 let deadline;
+
+
+function preload() {
+    this.load.image("loadlogo","atlas/Logo_Jcave.png");
+}
+
+function create() {
+    this.scene.start("bootGame");
+}
